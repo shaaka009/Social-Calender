a52340dd3cc4b9da1a984cb330abf403270aa6a6
Akaash Mahinth <akaash2003@gmail.com>
2025-09-28 22:29:54 -0400

    refactor(frontend): adjust ScreenWrapper styling & props

--- components/ScreenWrapper.jsx
+++ components/ScreenWrapper.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { theme } from "../constants/theme";
 
 const ScreenWrapper = ({ children, bg }) => {
   return (
     <SafeAreaView
-      style={[styles.container, { backgroundColor: bg }]}
+      style={[styles.container, { backgroundColor: bg ?? theme.colors.background }]}
       edges={["top"]}
     >
       <View style={styles.content}>{children}</View>
